0481bef3bdddb3c93f975af59d0541e31582d3e8
wjybxx <845740757@qq.com>
2023-12-23 19:04:24 +0800

    commons从bigcat分离

---
+++ tools/src/test/java/cn/wjybxx/common/temp/String2IntEnum.java
@@ -0,0 +1,47 @@
+package cn.wjybxx.common.temp;
+
+import cn.wjybxx.common.EnumLite;
+import cn.wjybxx.common.EnumLiteMap;
+import cn.wjybxx.common.EnumUtils;
+import cn.wjybxx.common.codec.binary.BinarySerializable;
+import java.util.List;
+
+@BinarySerializable
+public enum String2IntEnum implements EnumLite {
+    ONE(1),
+
+    TWO(2),
+
+    THREE(3);
+
+    public static final EnumLiteMap<String2IntEnum> MAPPER = EnumUtils.mapping(values());
+
+    public static final List<String2IntEnum> VALUES = MAPPER.values();
+
+    public static final int MIN_VALUE = 1;
+
+    public static final int MAX_VALUE = 3;
+
+    public final int number;
+
+    String2IntEnum(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public final int getNumber() {
+        return number;
+    }
+
+    public static String2IntEnum forNumber(int number) {
+        return MAPPER.forNumber(number);
+    }
+
+    public static String2IntEnum forNumber(int number, String2IntEnum def) {
+        return MAPPER.forNumber(number, def);
+    }
+
+    public static String2IntEnum checkedForNumber(final int number) {
+        return MAPPER.checkedForNumber(number);
+    }
+}
---
+++ tools/src/test/java/cn/wjybxx/common/temp/cfg2/String2StringConst.java
@@ -0,0 +1,45 @@
+package cn.wjybxx.common.temp.cfg2;
+
+import cn.wjybxx.common.CollectionUtils;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class String2StringConst {
+    public static final String ONE = "1";
+
+    public static final String TWO = "2";
+
+    public static final String THREE = "3";
+
+    public static final Map<String, String> VALUE_MAP;
+
+    public static final Set<String> VALUE_SET;
+
+    static {
+         {
+            Map<String, String> tempMap = new LinkedHashMap<>();
+            tempMap.put("ONE", "1");
+            tempMap.put("TWO", "2");
+            tempMap.put("THREE", "3");
+            VALUE_MAP = CollectionUtils.toImmutableLinkedHashMap(tempMap);
+        }
+        VALUE_SET = Set.copyOf(VALUE_MAP.values());
+    }
+
+    public static boolean isEmpty() {
+        return VALUE_MAP.isEmpty();
+    }
+
+    public static int size() {
+        return VALUE_MAP.size();
+    }
+
+    public static boolean containsKey(String key) {
+        return VALUE_MAP.containsKey(key);
+    }
+
+    public static boolean containsValue(String value) {
+        return VALUE_SET.contains(value);
+    }
+}
