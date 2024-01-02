d781bac7863438625ff9c4aec398634b08bea0e2
wjybxx <845740757@qq.com>
2024-01-02 11:08:30 +0800

    fix copyright 日期

--- csharp/Wjybxx.Commons.Core/src/Collections/PutBehavior.cs
+++ csharp/Wjybxx.Commons.Core/src/Collections/PutBehavior.cs
@@ -1,6 +1,6 @@
 ﻿#region LICENSE
 
-//  Copyright 2023 wjybxx
+//  Copyright 2023-2024 wjybxx
 // 
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
