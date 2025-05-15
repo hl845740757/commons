ed96c6e8728433c0b039bca53e2f78f71e9367ad
wjybxx <845740757@qq.com>
2025-05-15 17:51:06 +0800

    移植regulator...

--- java/Commons-Base/src/main/java/cn/wjybxx/base/time/GRegulator.java
+++ java/Commons-Base/src/main/java/cn/wjybxx/base/time/GRegulator.java
@@ -19,12 +19,14 @@ package cn.wjybxx.base.time;
 /**
  * 频率调节器 - 可理解为轮询式Timer调度器。
  * 它使用Timer的调度算法，但Timer是回调式的，Regulator是轮询式的。
+ * <p>
+ * 该实现是{@link Regulator}的特化实现，时间是double类型，用于游戏开发。
  *
  * @author wjybxx
  * date 2023/4/4
  */
 @SuppressWarnings("unused")
-public class Regulator {
+public class GRegulator {
 
     /** 仅执行一次 */
     private static final byte ONCE = 0;
@@ -36,25 +38,25 @@ public class Regulator {
     /** 其实可以省略，根据{@link #period}的正负判断，但为了提高可读性，还是不那么做 */
     private final byte type;
     /** 首次执行延迟 */
-    private long firstDelay;
+    private double firstDelay;
     /** 后期执行延迟 */
-    private long period;
+    private double period;
 
     /**
      * 它的真实含义取决于外部，可能是系统时间也可能不是，甚至可能是帧数
      * 在存储已触发次数的情况下还使用上次更新的时间戳，这使得可以在运行的过程中修改间隔，该实现是有状态的。
      * 注意：允许为负数，外部赋值什么就是什么。
      */
-    private long lastUpdateTime;
+    private double lastUpdateTime;
     /** 两次执行之间的间隔 */
-    private long deltaTime;
+    private double deltaTime;
     /** 已触发次数 */
     private int count;
 
     /** 关联的上下文 */
     private Object context;
 
-    private Regulator(byte type, long firstDelay, long period, long lastUpdateTime) {
+    private GRegulator(byte type, double firstDelay, double period, double lastUpdateTime) {
         this.type = type;
         this.firstDelay = firstDelay;
         this.period = period;
@@ -64,14 +66,14 @@ public class Regulator {
         this.count = 0;
     }
 
-    private static long checkFirstDelay(byte type, long firstDelay) {
+    private static double checkFirstDelay(byte type, double firstDelay) {
         if (type == FIX_RATE && firstDelay < 0) {
             throw new IllegalArgumentException("the firstDelay of fixRate must gte 0, delay " + firstDelay);
         }
         return firstDelay;
     }
 
-    private static long checkPeriod(long period) {
+    private static double checkPeriod(double period) {
         if (period <= 0) {
             throw new IllegalArgumentException("period must be positive, period " + period);
         }
@@ -83,8 +85,8 @@ public class Regulator {
      *
      * @param firstDelay 首次执行延迟
      */
-    public static Regulator newOnce(long firstDelay) {
-        return new Regulator(ONCE, firstDelay, 0, 0);
+    public static GRegulator newOnce(double firstDelay) {
+        return new GRegulator(ONCE, firstDelay, 0, 0);
     }
 
     /**
@@ -93,9 +95,9 @@ public class Regulator {
      * @param firstDelay 首次执行延迟
      * @param period     触发间隔
      */
-    public static Regulator newFixedDelay(long firstDelay, long period) {
+    public static GRegulator newFixedDelay(double firstDelay, double period) {
         checkPeriod(period);
-        return new Regulator(FIX_DELAY, firstDelay, period, 0);
+        return new GRegulator(FIX_DELAY, firstDelay, period, 0);
     }
 
     /**
@@ -104,10 +106,10 @@ public class Regulator {
      * @param firstDelay 首次执行延迟
      * @param period     触发间隔
      */
-    public static Regulator newFixedRate(long firstDelay, long period) {
+    public static GRegulator newFixedRate(double firstDelay, double period) {
         checkPeriod(period);
         checkFirstDelay(FIX_RATE, firstDelay);
-        return new Regulator(FIX_RATE, firstDelay, period, 0);
+        return new GRegulator(FIX_RATE, firstDelay, period, 0);
     }
 
     /**
@@ -117,7 +119,7 @@ public class Regulator {
      * @param curTime 当前时间
      * @return this
      */
-    public Regulator restart(long curTime) {
+    public GRegulator restart(double curTime) {
         lastUpdateTime = curTime;
         deltaTime = 0;
         count = 0;
@@ -128,7 +130,7 @@ public class Regulator {
      * @param curTime 当前时间
      * @return 如果应该执行一次update或者tick，则返回true，否则返回false
      */
-    public boolean isReady(long curTime) {
+    public boolean isReady(double curTime) {
         boolean ready = count == 0
                 ? (curTime - lastUpdateTime >= firstDelay)
                 : (period > 0 && (curTime - lastUpdateTime >= period));
@@ -145,12 +147,12 @@ public class Regulator {
      * @param curTime 当前时间
      * @return deltaTime
      */
-    public long forceReady(long curTime) {
+    public double forceReady(double curTime) {
         internalUpdate(curTime);
         return deltaTime;
     }
 
-    private void internalUpdate(long curTime) {
+    private void internalUpdate(double curTime) {
         if (type == FIX_DELAY || type == ONCE) {
             // 使用真实时间计算，但deltaTime也不能小于0
             deltaTime = Math.max(0, curTime - lastUpdateTime);
@@ -169,7 +171,7 @@ public class Regulator {
     }
 
     /** 获取下次执行的延迟 */
-    public long getDelay(long curTime) {
+    public double getDelay(double curTime) {
         if (count == 0) {
             return Math.max(0, curTime - lastUpdateTime - firstDelay);
         }
@@ -177,7 +179,7 @@ public class Regulator {
     }
 
     /** 校准时间 */
-    public void correctTime(long curTime) {
+    public void correctTime(double curTime) {
         this.lastUpdateTime = curTime;
     }
 
@@ -190,7 +192,7 @@ public class Regulator {
      * 获取上次成功更新的时间戳
      * 它的具体含义取悦于更新时使用的{@code curTime}的含义。
      */
-    public long getLastUpdateTime() {
+    public double getLastUpdateTime() {
         return lastUpdateTime;
     }
 
@@ -200,7 +202,7 @@ public class Regulator {
      *
      * @return 两次逻辑帧之间的间隔。
      */
-    public long getDeltaTime() {
+    public double getDeltaTime() {
         return deltaTime;
     }
 
@@ -214,24 +216,24 @@ public class Regulator {
         return context;
     }
 
-    public Regulator setContext(Object context) {
+    public GRegulator setContext(Object context) {
         this.context = context;
         return this;
     }
 
-    public long getFirstDelay() {
+    public double getFirstDelay() {
         return firstDelay;
     }
 
-    public void setFirstDelay(long firstDelay) {
+    public void setFirstDelay(double firstDelay) {
         this.firstDelay = checkFirstDelay(this.type, firstDelay);
     }
 
-    public long getPeriod() {
+    public double getPeriod() {
         return period;
     }
 
-    public void setPeriod(long period) {
+    public void setPeriod(double period) {
         this.period = checkPeriod(period);
     }
 
